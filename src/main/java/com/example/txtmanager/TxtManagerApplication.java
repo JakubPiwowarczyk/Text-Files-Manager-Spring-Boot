1dea7d9bae9ffcc1e5719651250fc077ca49d38c
JakubPiwowarczyk <s24820@pjwstk.edu.pl>
2024-02-27 10:42:12 +0100

    project initialized and configured

---
+++ src/main/java/com/example/txtmanager/TxtManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.txtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxtManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/txtmanager/TxtManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.txtmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxtManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
