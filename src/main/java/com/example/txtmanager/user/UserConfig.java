6d91d9cff9e3ccb6e772f66e425e08220dbea831
JakubPiwowarczyk <s24820@pjwstk.edu.pl>
2024-02-28 13:46:51 +0100

    created 'UserService' and added basic read operations

--- src/main/java/com/example/txtmanager/user/UserConfig.java
+++ src/main/java/com/example/txtmanager/user/UserConfig.java
@@ -4,19 +4,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class UserConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(UserRepository repo){
         return args -> {
-          User user = new User(
+          User user1 = new User(
                   "kuba@gmail.com",
                   "kuba",
                   "pass"
           );
+          User user2 = new User(
+                  "maciek@gmail.com",
+                  "maciek",
+                  "pilka"
+          );
 
-            repo.save(user);
+            repo.saveAll(List.of(user1, user2));
         };
     }
 
---
+++ src/main/java/com/example/txtmanager/user/UserService.java
@@ -0,0 +1,36 @@
+package com.example.txtmanager.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> getUserByNickname(String nickname) {
+        return userRepository.findByNickname(nickname);
+    }
+
+}
