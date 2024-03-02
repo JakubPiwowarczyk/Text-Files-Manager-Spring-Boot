9c1351d89c7c3cab871e7c3f628f2d4f4dca1950
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2024-03-02 17:48:18 +0100

    created test methods for findByNickname in 'UserRepositoryTest'

--- src/test/java/com/example/txtmanager/user/UserRepositoryTest.java
+++ src/test/java/com/example/txtmanager/user/UserRepositoryTest.java
@@ -56,4 +56,41 @@ public class UserRepositoryTest {
         // then
         assertThat(wasUserFound).isFalse();
     }
+
+    @Test
+    public void findByNickname_ShouldReturnUser_WhenUserExists() {
+        // given
+        String nickname = "JohnDoe";
+        User user1 = new User(
+                "john.doe@gmail.com",
+                nickname,
+                "johnny321"
+        );
+        User user2 = new User(
+                "joe.biden@gmail.com",
+                "JoeBiden",
+                "white-house111"
+        );
+        userRepository.saveAll(List.of(user1, user2));
+
+        // when
+        User foundUser = userRepository.findByNickname(nickname).orElse(null);
+
+        // then
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser).isEqualTo(user1);
+        assertThat(foundUser).isNotEqualTo(user2);
+    }
+
+    @Test
+    public void findByNickname_ShouldReturnEmptyOptional_WhenUserDoesNotExists() {
+        // given
+        String nickname = "JohnDoe";
+
+        // when
+        boolean wasUserFound = userRepository.findByNickname(nickname).isPresent();
+
+        // then
+        assertThat(wasUserFound).isFalse();
+    }
 }
