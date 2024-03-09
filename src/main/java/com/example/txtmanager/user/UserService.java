2421624e6834abd4964dae6c1fe048418e8099d8
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2024-03-09 17:02:28 +0100

    created method registerUser in 'UserService'

--- src/main/java/com/example/txtmanager/user/UserService.java
+++ src/main/java/com/example/txtmanager/user/UserService.java
@@ -1,14 +1,21 @@
 package com.example.txtmanager.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UserService {
+
+    @Value("${file-database.path}")
+    private String FILE_DATABASE_PATH;
 
     private final UserRepository userRepository;
 
@@ -31,6 +38,31 @@ public class UserService {
 
     public Optional<User> getUserByNickname(String nickname) {
         return userRepository.findByNickname(nickname);
+    }
+
+    public boolean isEmailInUse(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public boolean isNicknameInUse(String nickname) {
+        return userRepository.existsByNickname(nickname);
+    }
+
+    public boolean registerUser(UserRegisterDTO userToRegister) {
+        String email = userToRegister.email();
+        String nickname = userToRegister.nickname();
+        String password = userToRegister.password();
+
+        User user = new User(email, nickname, password);
+
+        try {
+            Files.createDirectory(Path.of(FILE_DATABASE_PATH + "/" + nickname));
+        } catch (IOException e) {
+            return false;
+        }
+
+        userRepository.save(user);
+        return true;
     }
 
 }
